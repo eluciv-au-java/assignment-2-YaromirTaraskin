4128914010956376e6fcbeb1d18388979aa0b7a3
Yaromir Taraskin <62649776+YaromirTaraskin@users.noreply.github.com>
2022-11-08 21:42:54 +0300

    added Point class

---
+++ src/main/java/edu/au/javacourse/transformation/Point.java
@@ -0,0 +1,29 @@
+package edu.au.javacourse.transformation;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Matrix vector){
+        this.x = vector.getValue(0, 0);
+        this.y = vector.getValue(0, 1);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Matrix asVector(){
+        double[][] protoMatrix = {{this.getX(), this.getY(), 1}};
+        return new Matrix(protoMatrix);
+    }
+}
