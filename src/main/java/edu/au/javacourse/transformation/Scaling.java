733a9ea5a68474a81474e3d411d1bcff9377b7e3
Yaromir Taraskin <62649776+YaromirTaraskin@users.noreply.github.com>
2022-11-08 22:55:58 +0300

    added Scaling class

---
+++ src/main/java/edu/au/javacourse/transformation/Scaling.java
@@ -0,0 +1,15 @@
+package edu.au.javacourse.transformation;
+
+public class Scaling extends AffineTransformation{
+    public Scaling(double kx, double ky, double kz){
+        double[][] scalingProtoMatrix = {
+                {kx, 0, 0},
+                {0, ky, 0},
+                {0, 0, kz}
+        };
+        this.matrix = new Matrix(scalingProtoMatrix);
+    }
+    public Scaling(double kx, double ky){
+        this(kx, ky, 1);
+    }
+}
